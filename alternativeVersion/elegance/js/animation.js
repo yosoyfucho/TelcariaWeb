6400b04d5fb29b65065643cf56a24b1517505561
Fucho <fucho@me.com>
2015-06-08 11:13:46 +0200

    añadido carrusel de partners

--- alternativeVersion/elegance/js/animation.js
+++ alternativeVersion/elegance/js/animation.js
@@ -20,4 +20,3 @@ $(window).scroll(function( about ) {
       }
     });
   });
-
--- alternativeVersion/elegance/js/carousels.js
+++ alternativeVersion/elegance/js/carousels.js
@@ -5,21 +5,21 @@ $(function(){
   var wrapwidth = (txtcount * 415) + 415; // 400px width for each testimonial item
   $txtcarousel.css('width',wrapwidth);
   var animtime = 750; // milliseconds for clients carousel
- 
+
   // prev & next btns for testimonials
   $('#prv-testimonial').on('click', function(){
     var $last = $('#testimonial-list li:last');
     $last.remove().css({ 'margin-left': '-415px' });
     $('#testimonial-list li:first').before($last);
-    $last.animate({ 'margin-left': '0px' }, animtime); 
+    $last.animate({ 'margin-left': '0px' }, animtime);
   });
-  
+
   $('#nxt-testimonial').on('click', function(){
     var $first = $('#testimonial-list li:first');
     $first.animate({ 'margin-left': '-415px' }, animtime, function() {
       $first.remove().css({ 'margin-left': '0px' });
       $('#testimonial-list li:last').after($first);
-    });  
+    });
   });
 
 
@@ -27,13 +27,13 @@ $(function(){
   // http://stackoverflow.com/questions/6759494/jquery-function-definition-in-a-carousel-script
   var $clientcarousel = $('#clients-list');
   var clients = $clientcarousel.children().length;
-  var clientwidth = (clients * 200); // 140px width for each client item 
+  var clientwidth = (clients * 200); // 140px width for each client item
   $clientcarousel.css('width',clientwidth);
-  
+
   var rotating = true;
   var clientspeed = 3000;
   var seeclients = setInterval(rotateClients, clientspeed);
-  
+
   $(document).on({
     mouseenter: function(){
       rotating = false; // turn off rotation when hovering
@@ -42,7 +42,7 @@ $(function(){
       rotating = true;
     }
   }, '#clients');
-  
+
   function rotateClients() {
     if(rotating != false) {
       var $first = $('#clients-list li:first');
