9e47742885a57cb7df8d48f8596961805a01e31a
Fucho <fucho@me.com>
2015-06-10 09:38:05 +0200

    generando textos en php

---
+++ pruebasPHP/directory/lang_en.php
@@ -0,0 +1,26 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+// Title
+$lang['TITLE_HOME'] = 'Telcaria Website - Home';
+$lang['TITLE_ABOUT'] = 'Telcaria Website - About Us';
+
+
+// Menu
+$lang['MENU_ABOUT_US'] = 'About Us';
+$lang['MENU_SERVICES'] = 'Services';
+$lang['MENU_RESEARCH'] = 'Research';
+$lang['MENU_WORK'] = 'Work With Us!';
+
+// http_request_method_name
+$lang['Home_HEADING'] = 'Home';
+$lang['MENU_SERVICES'] = 'Services';
+$lang['MENU_RESEARCH'] = 'Research';
+$lang['MENU_WORK'] = 'Work With Us!';
+?>
---
+++ pruebasPHP/directory/lang_sp.php
@@ -0,0 +1,18 @@
+<?php
+/*
+------------------
+Language: Spanish
+------------------
+*/
+
+$lang = array();
+
+
+// Menu
+
+
+$lang['MENU_ABOUT_US'] = 'Acerca de Nosotros';
+$lang['MENU_SERVICES'] = 'Servicios';
+$lang['MENU_RESEARCH'] = 'Investigación';
+$lang['MENU_WORK'] = 'Trabaja con nosotros';
+?>
