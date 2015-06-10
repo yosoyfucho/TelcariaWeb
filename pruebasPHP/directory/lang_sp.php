04724e1142670ff5bb760aa5eb8660966a838354
Fucho <fucho@me.com>
2015-06-10 11:52:58 +0200

    cmabios varios

--- pruebasPHP/directory/lang_sp.php
+++ pruebasPHP/directory/lang_sp.php
@@ -7,10 +7,15 @@ Language: Spanish
 
 $lang = array();
 
+// Title
+$lang['TITLE_HOME'] = 'Telcaria Website - Inicio';
+$lang['TITLE_ABOUT'] = 'Telcaria Website - Sobre nosotros';
+$lang['TITLE_SERVICES'] = 'Telcaria Website - Servicios';
+$lang['TITLE_RESEARCH'] = 'Telcaria Website - Investigación';
+$lang['TITLE_WORK'] = 'Telcaria Website - Trabajo';
+$lang['TITLE_CONTACT'] = 'Telcaria Website - Contacto';
 
 // Menu
-
-
 $lang['MENU_ABOUT_US'] = 'Acerca de Nosotros';
 $lang['MENU_SERVICES'] = 'Servicios';
 $lang['MENU_RESEARCH'] = 'Investigación';
